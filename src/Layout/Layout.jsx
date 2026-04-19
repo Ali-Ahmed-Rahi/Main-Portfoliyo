63917653b1eb72e2ed5ff3385cb0caf2b87884a7
rahiji9 <aliahmedrahi007@gmail.com>
2026-04-19 22:16:41 +0600

    layout change

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -5,22 +5,24 @@ import ResponsiveContact from "../Dual/ResponsiveContact";
 
 const Layout = () => {
   return (
-    <div className="max-w-7xl mx-auto md:flex shadow-[0_25px_50px_rgba(0,0,0,0.5)] ">
-      {/* the sidebar for lg device */}
-      <div className=" bg-black md:h-screen md:fixed md:top-0 hidden md:block ">
-        <Sidebar />
+    <section className="bg-black backdrop:blur-lg">
+      <div className="max-w-7xl mx-auto md:flex shadow-[0_25px_50px_rgba(0,0,0,0.5)] ">
+        {/* the sidebar for lg device */}
+        <div className=" bg-black md:h-screen md:fixed md:top-0 hidden md:block border-l md:border-yellow-600">
+          <Sidebar />
+        </div>
+        {/* the sidebar for sm device */}
+        <div className="md:hidden block">
+          <ResponsiveSb />
+        </div>
+        <div className=" bg-black md:ml-[282px] overflow-y-auto md:px-10 px-2 mb-[70px] md:mb-0 min-h-screen">
+          <Outlet />
+        </div>
+        <div className="md:hidden block ">
+          <ResponsiveContact></ResponsiveContact>
+        </div>
       </div>
-      {/* the sidebar for sm device */}
-      <div className="md:hidden block">
-        <ResponsiveSb />
-      </div>
-      <div className=" bg-black md:ml-[282px] overflow-y-auto md:px-10 px-2 mb-[70px] md:mb-0 min-h-screen">
-        <Outlet />
-      </div>
-      <div className="md:hidden block ">
-        <ResponsiveContact></ResponsiveContact>
-      </div>
-    </div>
+    </section>
   );
 };
 
